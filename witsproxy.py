faf442a0c68daa812f2ad08c6c53ffb2e48821b2
Mandla Mbuli <lm.mbuli@gmail.com>
2012-09-08 13:17:07 +0200

    implemented support for configuring cntlm

--- witsproxy.py
+++ witsproxy.py
@@ -1,14 +1,16 @@
 #! /usr/bin/env python
 
-des = """ 
-witsproxy: Sets the Wits proxy for all the files that I have to keep setting it for \n
-
-author: Leonard Mandla Mbuli <lm.mbuli@gmail.com> \n
-
-creation date: 08 July 2011 \n
-last update: 06 September 2012 \n
-python 3 \n
-
+descr = """ 
+    witsproxy 
+    Sets the Wits proxy for all the files that I have to 
+    keep setting it for \n
+    
+    author: Leonard Mandla Mbuli <lm.mbuli@gmail.com> \n
+    
+    creation date: 08 July 2011 \n
+    last update: 06 September 2012 \n
+    
+    python 3 \n
 """
 
 import argparse
@@ -16,39 +18,49 @@ import re
 import os
 import sys
 import fileinput
+import textwrap
 
 def main():
     """ The main function"""
-    # des = """Sets the Wits proxy for me, how kind don't you think.\
-    #       Needs to be run as root though """
-    parser = argparse.ArgumentParser(description=des) 
+    parser = argparse.ArgumentParser(
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            description=textwrap.dedent(descr)
+            ) 
     parser.add_argument('username',
-                    help="The new or updated proxy username")
+                    help="Your student or staff number, without ds\\ or students\\")
     parser.add_argument('password', 
                     help="The new or updated proxy password")
-    parser.add_argument('-a', '--apt', action='store_true', 
+    parser.add_argument('-a', '--set_apt', action='store_true', 
                     help="Set only the apt proxy (Ubuntu or the like)")
-    parser.add_argument('-b', '--bash', action='store_true', 
+    parser.add_argument('-b', '--set_bash', action='store_true', 
                     help="Set the bash proxy")
-    parser.add_argument('-c', '--cntlm', action='store_true', 
-                    help="Use CNTML to set the proxy (MUST ALREADY BE INSTALLED and configured to listen on port 8000)")
-    parser.add_argument('-s', '--staff', action='store_true', 
+    parser.add_argument('-s', '--set_svn', action='store_true', 
+                    help="To be implemented soon")
+    parser.add_argument('-C', '--use_cntlm', action='store_true', 
+                    help="Use CNTML to set the proxy (CNTML must installed but not configured)")
+    parser.add_argument('-S', '--is_staff', action='store_true', 
                     help="Change proxy to staff proxy")
     args = parser.parse_args()
-    proxystr = "http://"+args.username+":"+args.password
 
-    if(args.staff):
-        proxystr += "@proxyad.wits.ac.za:80/"
-    elif(args.cntlm == True):
-        # TODO: write the configuration file for CNTLM
-        proxystr = "127.0.0.1:8000"
-    else:
-        proxystr += "@proxyss.wits.ac.za:80/"
-
-    if args.bash == True:
+    proxystr,proxyaddress = make_proxystr(args.username,args.password,args.use_cntlm,args.is_staff)
+    if(args.use_cntlm):
+        set_cntlm_config(args.username,args.password,proxyaddress,args.is_staff);
+    if args.set_bash:
         set_bash_proxy(proxystr)
-    if args.apt == True:
+    if args.set_apt:
         set_apt_proxy(proxystr)
+
+def make_proxystr(username,password,cntlm,staff):
+    proxystr = "http://"
+    proxyaddress = "proxyss.wits.ac.za:80"
+    if(staff):
+        proxyaddress = "proxyad.wits.ac.za:80"
+        proxystr += "http://ds\\"+username+":"+password+"@"+proxyaddress
+    elif(cntlm):
+        proxystr += "127.0.0.1:8000"
+    else:
+        proxystr += "http://students\\"+username+":"+password+"@"
+    return proxystr,proxyaddress
 
 def set_apt_proxy(proxystr):
     """ 
@@ -64,6 +76,57 @@ def set_apt_proxy(proxystr):
     f.write(https_proxy)
     f.write(ftp_proxy)
     f.close()
+
+def set_cntlm_config(username,password,address,is_staff):
+    """ 
+        Set the proxy details in the cntlm config 
+    """
+    domain = "students"
+    if(is_staff):
+        domain = "ds"
+    no_proxy="*wits.ac.za,*mirror.ac.za"
+    config_file = "/etc/cntlm.conf"
+
+    if os.path.isfile(config_file):
+        replacements = 0
+        for line in fileinput.input(config_file, inplace = True):
+            if re.match(r'Username', line)!= None:
+                replacements = replacements + 1
+                sys.stdout.write('{0:11} {1}\n'.format("Username",username))
+            elif re.match(r'Domain', line)!= None:
+                sys.stdout.write('{0:11} {1}\n'.format("Domain",domain))
+                replacements = replacements + 1
+            elif re.match(r'Password', line)!= None:
+                sys.stdout.write('{0:11} {1}\n'.format("Password",password))
+                replacements = replacements + 1
+            elif re.match(r'Proxy', line)!= None:
+                sys.stdout.write('{0:11} {1}\n'.format("Proxy",address))
+                replacements = replacements + 1
+            elif re.match(r'NoProxy', line)!= None:
+                line = line.rstrip("\n")
+                sys.stdout.write(line+","+no_proxy+"\n")
+                replacements = replacements + 1
+            elif re.match(r'Listen', line)!= None:
+                sys.stdout.write('{0:11} {1}\n'.format("Listen","8000"))
+                replacements = replacements + 1
+            else:
+                sys.stdout.write(line)
+        if replacements == 0:
+           f = open(config_file, 'a')
+           details = {"Username":username,"Domain":domain,"Password":password,"Proxy":address,"Listen":"8000"}
+           for detail,value in details.items():
+               f.write('{0:11} {1}\n'.format(detail,value))
+           f.close()
+           print("Proxy settings not found, thus appended")
+        elif replacements == 5:
+           print("Proxy settings successfully changed")
+    else:
+        f = open(config_file, 'w')
+        details = {"Username":username,"Domain":domain,"Password":password,"Proxy":address,"Listen":"8000"}
+        for detail,value in details.items():
+            f.write('{0:11} {1}\n'.format(detail,value))
+        f.close()
+        print("Created new '"+config_file+"' with new proxy settings")
 
 def set_bash_proxy(proxystr):
     """ 
