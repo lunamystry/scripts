873658fbcbcc2bb581078ef082cbf7e2ad389855
Mandla Mbuli <lm.mbuli@gmail.com>
2014-01-15 15:06:04 +0200

    Lets get started

---
+++ lyconfig/__main__.py
@@ -0,0 +1,14 @@
+import kivy
+kivy.require('1.7.1')
+
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+
+class MainView(AnchorLayout):
+    pass
+
+class Lyconfig(App):
+    pass
+
+
+Lyconfig().run()
