830fc23915f2225bd5af835d4932a58264c2a93d
Mandla Mbuli <lm.mbuli@gmail.com>
2014-01-19 05:25:29 +0200

    using a custom list item and adapter

--- lyconfig/__main__.py
+++ lyconfig/__main__.py
@@ -3,12 +3,36 @@ kivy.require('1.7.2')
 
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.listview import ListItemButton
+from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
+import os
+
+
+class FileListItem(BoxLayout, ListItemButton):
+    filename = StringProperty()
+
+class FileList(BoxLayout):
+    list_view = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(FileList, self).__init__()
+        self.list_view.adapter.data = os.listdir("/home/leny/System/Wallpapers")
+
+    def args_converter(self, index, id):
+        return {}
+
 
 class MainView(AnchorLayout):
-    pass
+    Wallpaper_dir = StringProperty()
+
 
 class Lyconfig(App):
-    pass
+    def build(self):
+        main_view = MainView()
+        return main_view
 
 
-Lyconfig().run()
+if __name__ == "__main__":
+    Lyconfig().run()
