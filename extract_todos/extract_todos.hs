2cad9b49f5af0b318e878848fa053b8adfaefdf5
Mandla Mbuli <lm.mbuli@gmail.com>
2014-06-16 22:09:46 +0000

    added type signatures

--- extract_todos/extract_todos.hs
+++ extract_todos/extract_todos.hs
@@ -1,9 +1,11 @@
 import System.Environment (getArgs)
 
+main :: IO ()
 main = do
   args <- getArgs
   mapM_ find_todos args
 
 
+find_todos:: String -> IO ()
 find_todos arg = do
     putStrLn ("Hello " ++ arg)
