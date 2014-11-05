c0f14582c69125f92bf7068a0d544f179e3c9749
Mandla Mbuli <lm.mbuli@gmail.com>
2014-11-05 10:06:22 +0200

    clean up

---
+++ wordsearch/word.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+"""
+author: Leonard Mbuli <mail@mandla.me>
+
+creation date: 17 July 2014
+update date: 5 November 2014
+
+"""
+from utils import *
+
+class Word():
+    def __init__(self, text, direction, start, grid):
+        # The word must be stripped of hyphens and numbers
+        # x and y must be on the grid and the length must be less than grid
+        # The word must be lowercase
+        self.text = text
+        self.direction = direction
+        self.start = start
+        self.points = self._calculate_points();
+
+    def _calculate_points(self):
+        row_incr, col_incr = self._increments()
+        points = []
+        row = self.start.row
+        col = self.start.col
+        points.append(Point(row, col))
+        for letter in self.text[1:]:
+            row += row_incr
+            col += col_incr
+            if (row < 0 or col < 0 or row > len(grid) or col > len(grid[0])):
+                logging.debug("row:{0} col:{1}".format(row, col))
+                raise IndexError(self.text + " is not completely inside grid")
+            points.append(Point(row, col))
+        return points
+
+    def _increments(self):
+        col_incr = 0
+        row_incr = 0
+        if self.direction == 'EAST':
+            col_incr = 1
+        elif self.direction == 'WEST':
+            col_incr = -1
+        elif self.direction == 'SOUTH':
+            row_incr = 1
+        elif self.direction == 'NORTH':
+            row_incr = -1
+        elif self.direction == 'SOUTHEAST':
+            row_incr = 1
+            col_incr = 1
+        elif self.direction == 'SOUTHWEST':
+            row_incr = 1
+            col_incr = -1
+        elif self.direction == 'NORTHEAST':
+            row_incr = -1
+            col_incr = 1
+        elif self.direction == 'NORTHWEST':
+            row_incr = -1
+            col_incr = -1
+        return row_incr, col_incr
