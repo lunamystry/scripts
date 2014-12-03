186182837d2fcc2c790644a025208391d1fd82ec
Mandla Mbuli <lm.mbuli@gmail.com>
2014-12-03 15:27:57 +0200

    lets make it a Python 2.7 application

--- funamagama/main.py
+++ funamagama/main.py
@@ -6,6 +6,9 @@ creation date: 17 July 2014
 update date: 2 November 2014
 
 """
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import logging
 from grid import *
 
--- funamagama/point.py
+++ funamagama/point.py
@@ -6,6 +6,9 @@ creation date: 17 July 2014
 update date: 5 November 2014
 
 """
+from __future__ import print_function
+from __future__ import unicode_literals
+
 from collections import namedtuple
 
 Point = namedtuple("Point", "row col")
