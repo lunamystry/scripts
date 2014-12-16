d4c0115f9f511d55ae424cbdb3bc0254bc99f749
Mandla Mbuli <lm.mbuli@gmail.com>
2014-12-16 19:03:35 +0200

    added solver

---
+++ funamagama/solve/__init__.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+"""
+author: Leonard Mbuli <mail@mandla.me>
+
+A package for creating word search puzzles for hours of fun.
+The grid module is the main thing that is needed with the other
+modules being helper modules.
+
+creation date: 16 December 2014
+
+"""
+
