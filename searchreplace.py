11dad7643e3225941f2eeca3493f31a57944149d
Mandla Mbuli <lm.mbuli@gmail.com>
2015-01-10 12:27:42 +0200

    conform with PEP8

--- searchreplace.py
+++ searchreplace.py
@@ -17,57 +17,65 @@ import glob
 import argparse
 import textwrap
 
+
 def main():
     """ Does everything """
     parser = argparse.ArgumentParser(
-                formatter_class=argparse.RawDescriptionHelpFormatter,
-                description=textwrap.dedent(descr)
-            )
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent(descr)
+        )
     parser.add_argument('searchterm',
-                    help="The term to search for")
+                        help="The term to search for")
     parser.add_argument('replaceterm',
-                    help="The term to replace the search term with")
+                        help="The term to replace the search term with")
     parser.add_argument('-d', '--directory', action='store', default=".",
-                    help="The directory to search within, default is current directory")
+                        help="The directory to search within, default is \
+                                current directory")
     parser.add_argument('-e', '--extension', action='store', default="",
-                    help="Search only for files matching the extension")
+                        help="Search only for files matching the extension")
     parser.add_argument('-f', '--filename', action='store',
-                    help="Search and replace only within specified file")
+                        help="Search and replace only within specified file")
     parser.add_argument('-r', '--recursive', action='store_true',
-                    help="When searching in a directory, where to decend into sub directories")
+                        help="When searching in a directory, where to decend \
+                        into sub directories")
     args = parser.parse_args()
     print("SEARCH: "+args.searchterm)
     print("REPLACE: "+args.replaceterm)
 
-    if args.filename == None:
-        filenames = find_filenames(args.directory,args.extension,args.recursive)
+    if args.filename is None:
+        filenames = find_filenames(args.directory,
+                                   args.extension,
+                                   args.recursive)
         for filename in filenames:
-            search_replace(args.searchterm,args.replaceterm,filename)
+            search_replace(args.searchterm, args.replaceterm, filename)
     else:
-        search_replace(args.searchterm,args.replaceterm,args.filename)
+        search_replace(args.searchterm, args.replaceterm, args.filename)
 
-def find_filenames(directory,extension,is_recursive):
+
+def find_filenames(directory, extension, is_recursive):
     "searches either the provided directory for filenames "
     paths = glob.glob(directory+"/*")
     filenames = []
     for path in paths:
         if os.path.isdir(path) and is_recursive:
-            filenames += find_filenames(path,extension,is_recursive)
+            filenames += find_filenames(path, extension, is_recursive)
         else:
             filenames.append(path)
 
     if extension != "":
-        filenames = [filename for filename in filenames if os.path.splitext(filename)[1] == "."+extension]
+        filenames = [filename for filename in filenames
+                     if os.path.splitext(filename)[1] == "."+extension]
 
     return filenames
 
-def search_replace(searchterm,replaceterm,filename):
+
+def search_replace(searchterm, replaceterm, filename):
     if os.path.isfile(filename):
         replacements = 0
-        for line in fileinput.input(filename, inplace = True):
-            if re.match(r''+searchterm, line)!= None:
+        for line in fileinput.input(filename, inplace=True):
+            if re.match(r''+searchterm, line) is not None:
                 replacements = replacements + 1
-                line = line.replace(searchterm,replaceterm)
+                line = line.replace(searchterm, replaceterm)
                 sys.stdout.write(line)
             else:
                 sys.stdout.write(line)
