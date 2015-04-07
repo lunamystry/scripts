65bda5fbdbe03724d7d3f4c4a8b5edf96e410024
Mandla Mbuli <lm.mbuli@gmail.com>
2015-04-07 11:48:10 +0200

    Added script to count number of words in python files in a directory

---
+++ word_count.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+'''
+    simple script to count the number of lines in a python file, done for the 
+    Gauteng Python User Group
+
+    author: Mandla Mbuli <mail@mandla.me>
+    date: 7 Apr 2015
+'''
+from __future__ import print_function
+import os
+
+
+def main():
+    '''The main function, go C++'''
+    for root, folders, filenames in os.walk('.'):
+        for filename in filenames:
+            if filename.endswith('.py'):
+                with open(os.path.join(root, filename)) as f:
+                    print(len(f.readlines()), end='\tin ')
+                    print(os.path.join(root, filename))
+
+if __name__ == '__main__':
+    main()
