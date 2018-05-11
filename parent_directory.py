7ab3595c2fcfc4b164ffb5ce3baf6c238ca7ba2b
Mandla Mbuli <mail@mandla.me>
2018-05-11 06:59:11 +0200

    Add parent directory script

--- parent_directory.py
+++ parent_directory.py
@@ -14,13 +14,17 @@ import lxml.html
 import urllib.request, urllib.error, urllib.parse
 import logging
 import argparse
+import os
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
+# construct a namespace dictionary to pass to the xpath() call
+# this lets us use regular expressions in the xpath
+ns = {'re': 'http://exslt.org/regular-expressions'}
 
 url = "http://dept.ee.wits.ac.za/~cheng/ELEN4017/tuts/"
 save_path = "/home/leny/University/networkfun/tutorials/"
 
-def download():
+def download(url, save_path, reg, ns=ns):
     # fetch the page
     res = urllib.request.urlopen(url)
 
@@ -28,23 +32,26 @@ def download():
     # parse the response into an xml tree
     tree = lxml.html.fromstring(page)
 
-    # construct a namespace dictionary to pass to the xpath() call
-    # this lets us use regular expressions in the xpath
-    ns = {'re': 'http://exslt.org/regular-expressions'}
+    if (not os.path.isdir(save_path)):
+        os.makedirs(save_path)
+
+
+    reg = fr'//a[re:test(@href, "{reg}", "i")]'
 
     # iterate over all <a> tags whose href ends in ".pdf" (case-insensitive)
-    for node in tree.xpath('//a[re:test(@href, "\.pdf$", "i")]', namespaces=ns):
+    for node in tree.xpath(reg, namespaces=ns):
         filename = node.attrib['href']
         file_url = urllib.parse.urljoin(url, filename)
+        local_filename = os.path.join(save_path, filename)
 
-        logging.info("Processing: " + file_url)
+        logging.info(f'Processing: {file_url}')
 
         remote_file = urllib.request.urlopen(file_url)
-        local_file = open(save_path + filename, 'wb') # binary open file
+        local_file = open(local_filename, 'wb') # binary open file
         local_file.write(remote_file.read())
         local_file.close()
 
-        logging.info("Saved file: " + save_path + filename)
+        logging.info(f'Saved: {local_filename}')
 
 
 if __name__ == '__main__':
@@ -56,8 +63,7 @@ if __name__ == '__main__':
 
     parser.add_argument('download_from', help='The URL to download from')
     parser.add_argument('save_to', help='The directory to save to')
-    parser.add_argument('-f', '--filter', help='Regex used for filtering')
+    parser.add_argument('--has', help='Regex used for filtering. Files matching are kept')
 
     args = parser.parse_args()
-    logging.info(args)
-    # download()
+    download(args.download_from, args.save_to, args.has)
