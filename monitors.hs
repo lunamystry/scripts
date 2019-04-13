e37fd9f2cb764b503c313035bbd538b43543d271
Mandla Mbuli <mandla.mbuli@momentum.co.za>
2019-04-13 10:19:18 +0200

    Add script to change monitors

---
+++ monitors.hs
@@ -0,0 +1,47 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-13.15 script
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import Prelude hiding (FilePath)
+import Turtle
+import qualified Data.List as L
+import qualified Data.Text as DT
+import qualified Control.Foldl as Fold
+
+pathToText :: FilePath -> Text
+pathToText = format fp
+
+wallpaper :: FilePath -> FilePath
+wallpaper base = base </> "Images/wallpapers/wallpaper"
+
+layoutsDir :: FilePath -> FilePath
+layoutsDir base = base </> ".screenlayout"
+
+setWallpaper :: FilePath -> Shell ()
+setWallpaper w = procs "feh" ["--bg-scale", pathToText w] empty
+
+getLayoutsScripts :: [Text] -> FilePath -> IO [FilePath]
+getLayoutsScripts args dir =
+  case args of
+    [x] -> fold (find (contains $ text x) dir) Fold.list
+    x:xs -> fold (find (contains $ text x) dir) Fold.list
+    _ -> fold (ls dir) Fold.list
+
+filterScripts :: [FilePath] -> Shell Line
+filterScripts scripts = do
+  inshell (pipeToFzf scripts) empty
+  where
+    listToText xs =  DT.pack $ concat $ L.intersperse "\n" $ encodeString <$> xs
+    pipeToFzf names = "echo \"" <> listToText names <> "\" | fzf -1"
+
+main :: IO ()
+main = sh $ do
+  args <- arguments
+  homeDir <- home
+  scripts <- liftIO $ getLayoutsScripts args $ layoutsDir homeDir
+  case scripts of
+    [] -> echo "No matching layouts"
+    [x] -> shells (pathToText x) empty
+    xs -> (filterScripts xs) >>= \s -> shells (lineToText s) empty
+  setWallpaper $ wallpaper homeDir
