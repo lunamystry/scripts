6c9629bae9cd9dffce00e39717e8c549afce972f
Mandla Mbuli <mail@mandla.me>
2019-09-22 19:41:46 +0200

    Need to clean this script

--- cowsay.sh
+++ cowsay.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # The following can be added to the end of ~/.bashrc or /etc/bash.bashrc
@@ -6,24 +5,24 @@
 
 # Check for interactive terminal (should not produce output otherwise).
 #if [ "$PS1" ]; then
-  # Set location of cow files.
-  cowdir="/usr/share/cows"
+# Set location of cow files.
+cowdir="/usr/share/cows"
 
-  # Create an array from the files in our $cowdir.
-  cows=($cowdir/*)
+# Create an array from the files in our $cowdir.
+cows=($cowdir/*)
 
-  # Randomly select a cow from our cows 
-  # (${#myarr[*]} == num items in array, RANDOM == random number).
-  mycow="${cows[RANDOM % ${#cows[*]}]}"
-  # mycow="$cowdir/bunny.cow"
+# Randomly select a cow from our cows
+# (${#myarr[*]} == num items in array, RANDOM == random number).
+mycow="${cows[RANDOM % ${#cows[*]}]}"
+# mycow="$cowdir/bunny.cow"
 
-  # Create an array of possible cow actions (think or say).
-  cowactions=("cowsay" "cowthink")
+# Create an array of possible cow actions (think or say).
+cowactions=("cowsay" "cowthink")
 
-  # Randomly select an action.
-  myaction="${cowactions[RANDOM % ${#cowactions[*]}]}"
-  # myaction="cowsay"
+# Randomly select an action.
+myaction="${cowactions[RANDOM % ${#cowactions[*]}]}"
+# myaction="cowsay"
 
-  # Call fortune and pass the results to cowthink/cowsay with our chosen cow.
-  fortune | $myaction -n -f "`basename ${mycow}`"
+# Call fortune and pass the results to cowthink/cowsay with our chosen cow.
+fortune | $myaction -n -f "$(basename ${mycow})"
 #fi
